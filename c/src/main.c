5baf6a071122b3e214764f12a3070b836ee743ff
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2022-02-27 18:42:14 -0500

    æ

--- c/src/main.c
+++ c/src/main.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <ctype.h>
 #include "json.h"
+#include <zip.h>
 
 #define STRING_EQUAL(s1, s2) strcmp(s1, s2) == 0
 struct memory {
@@ -40,7 +41,7 @@ void setup_curl(CURL *curl, const char* url, struct memory* chunk) {
 const char* fetch_path(char* path) {
   FILE* file = fopen("gtag_path.txt", "r");
   if (file == NULL) {
-    printf("error: unknown gtag path\nrun %s setup [gtag path]");
+    printf("error: unknown gtag path\nrun cligmm setup [gtag path]");
     exit(1);
     return NULL;
   }
@@ -102,23 +103,27 @@ int main(int argc, char *argv[])
 
         res = curl_easy_perform(curl);
 
-        if (res != CURLE_OK)
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+        if (res != CURLE_OK) {
+          fprintf(stderr, "curl_easy_perform() failed: %s\n",
               curl_easy_strerror(res));
-        curl_easy_cleanup(curl);
+          return 1;
+        }
+            
+        
         char* cmdname = argv[1];
         json_object* modinforaw = json_tokener_parse(chunk.response);
-        
+        curl_easy_cleanup(curl);
         if (STRING_EQUAL(cmdname,"list")) {
           printf("modinfos:\n");
           
           array_list* modinfo = json_object_get_array(modinforaw);
           for (int i = 0; i < modinfo->length; i++) {
             json_object* obj = (struct json_object*) modinfo->array[i];
-            const char* name = mangle_name(json_object_get_string(json_object_object_get(obj, "name")));
+            const char* name = mangle_name((char *) json_object_get_string(json_object_object_get(obj, "name")));
             const char* version = json_object_get_string(json_object_object_get(obj, "version"));
             printf("%s %s\n", name, version);
           }
+          
         } else if (STRING_EQUAL(cmdname, "setup")) {
           if (argc < 3) {
             printf("usage: %s setup [path to gtag]\n", argv[0]);
@@ -138,8 +143,64 @@ int main(int argc, char *argv[])
           mangle_name(modname);
           array_list* modinfo = json_object_get_array(modinforaw);
           json_object* mod = find_mod(modinfo, modname);
-          
+          const char* version = json_object_get_string(json_object_object_get(mod, "version"));
+          const char* author = json_object_get_string(json_object_object_get(mod, "author"));
+          printf("Installing %s:\n    version: %s\n    author: %s\nProceed? (y/n)", modname, version, author);
+          char response = getc(stdin);
+          if (response != 'y') {
+            printf("denied... aborting");
+            return 0;
+          }
+          free(modname);
+          const char* url = json_object_get_string(json_object_object_get(mod, "download_url"));
+          CURL* zipcurl = curl_easy_init();
+          struct memory zipchunk = {0};
+          setup_curl(zipcurl, url, &zipchunk);
+          // reusing this... i'm tired
+          res = curl_easy_perform(zipcurl);
+
+          if (res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+            return 1;
+          }
+          char path[1000]; 
+          fetch_path(path);
+          strcat(path, "temp.zip");
+          FILE* zipfile = fopen(path, "w");
+          fputs(zipchunk.response, zipfile);
+          fclose(zipfile);
+          int error = 0;
+          zip_t* archive = zip_open(path, ZIP_RDONLY, &error);
+          int numentries = zip_get_num_entries(archive, 0);
+          for (int i = 0; i < numentries; i++) {
+            zip_file_t* entry = zip_fopen_index(archive, i, 0);
+            zip_stat_t entryinfo = {0};
+            zip_stat_init(&entryinfo);
+            zip_stat_index(archive, i, 0, &entryinfo);
+            if ((entryinfo.valid & ZIP_STAT_NAME) == 0 || (entryinfo.valid & ZIP_STAT_SIZE) == 0) {
+              printf("invalid entry");
+              return 1;
+            }
+            printf("Saving %s", entryinfo.name);
+            // this shit better free when it loops
+            char path2[1000];
+            fetch_path(path2);
+            strcat(path2, entryinfo.name);
+            // this will crash somewhere. too bad!
+            void* buffer = malloc(entryinfo.size);
+            // pray
+            zip_fread(entry, buffer, entryinfo.size);
+            FILE* savedfile = fopen(path2, "wb");
+            // this conversion is really fucking busted!!!
+            // fix me
+            fputs((const char*) buffer, savedfile);
+            fclose(savedfile);
+            free(buffer);
+          }
+          printf("Done!");
         }
+        
     } else {
         // idfk
         return 1;
