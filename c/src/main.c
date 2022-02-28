5ede9401249f5fff77e089fe6c9652353f8c6d85
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2022-02-28 12:14:03 -0500

    it works

--- c/src/main.c
+++ c/src/main.c
@@ -6,6 +6,8 @@
 #include <ctype.h>
 #include "json.h"
 #include <zip.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #define STRING_EQUAL(s1, s2) strcmp(s1, s2) == 0
 struct memory {
@@ -37,6 +39,22 @@ void setup_curl(CURL *curl, const char* url, struct memory* chunk) {
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, cb);
 
   curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)chunk);
+  // i'm going to kill someone
+  // it didn't work because redirect
+  curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION,1);
+}
+void setup_saved_curl(CURL *curl, const char* url, FILE* file) {
+  curl_easy_setopt(curl, CURLOPT_URL, url);
+  // don't cache
+  // curl_easy_setopt(curl, CURLOPT_ALTSVC, "altsvc.txt");
+  curl_easy_setopt(curl, CURLOPT_ALTSVC_CTRL, (long) CURLALTSVC_H1|CURLALTSVC_H2|CURLALTSVC_H3);
+
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)file);
+  // i'm tired
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, fwrite);
+  // i'm going to kill someone
+  // it didn't work because redirect
+  curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION,1);
 }
 const char* fetch_path(char* path) {
   FILE* file = fopen("gtag_path.txt", "r");
@@ -70,6 +88,30 @@ const char* mangle_name(char* name) {
   }
   
   return name;
+}
+
+const char * path_directory(const char* path) {
+  char * path2 = malloc(strlen(path) + 1);
+  strcpy(path2, path);
+  int i = strlen(path) - 1;
+  while (path[i] != '/') {
+    i--;
+  }
+  path2[i] = '\0';
+  return (const char*) path2;
+}
+void ensure_has_folder(const char* path) {
+  
+  if (access(path, F_OK) == 0) {
+    return;
+  } else {
+    const char* parent = path_directory(path);
+    ensure_has_folder(parent);
+    printf("Creating directory %s\n", path);
+    mkdir(path, ACCESSPERMS);
+    free((void *) parent);
+    
+  }
 }
 // modname expected to be mangled
 struct json_object* find_mod(array_list* modlist, const char* modname) {
@@ -147,31 +189,39 @@ int main(int argc, char *argv[])
           const char* author = json_object_get_string(json_object_object_get(mod, "author"));
           printf("Installing %s:\n    version: %s\n    author: %s\nProceed? (y/n)", modname, version, author);
           char response = getc(stdin);
+          printf("\n");
           if (response != 'y') {
-            printf("denied... aborting");
+            printf("denied... aborting\n");
             return 0;
           }
           free(modname);
           const char* url = json_object_get_string(json_object_object_get(mod, "download_url"));
           CURL* zipcurl = curl_easy_init();
-          struct memory zipchunk = {0};
-          setup_curl(zipcurl, url, &zipchunk);
+          char path[1000]; 
+          fetch_path(path);
+          strcat(path, "temp.zip");
+
+          FILE* zipfile = fopen(path, "wb");
+          setup_saved_curl(zipcurl, url, zipfile);
+          
           // reusing this... i'm tired
           res = curl_easy_perform(zipcurl);
-
-          if (res != CURLE_OK) {
+          // close it AFTER it's been saved
+          fclose(zipfile);
+          if (res != CURLE_OK) {  
             fprintf(stderr, "curl_easy_perform() failed: %s\n",
                 curl_easy_strerror(res));
             return 1;
           }
-          char path[1000]; 
-          fetch_path(path);
-          strcat(path, "temp.zip");
-          FILE* zipfile = fopen(path, "w");
-          fputs(zipchunk.response, zipfile);
-          fclose(zipfile);
+          
+         
+
           int error = 0;
           zip_t* archive = zip_open(path, ZIP_RDONLY, &error);
+          if (error != 0) {
+            printf("failed: %d\n", error);
+            return 1;
+          }
           int numentries = zip_get_num_entries(archive, 0);
           for (int i = 0; i < numentries; i++) {
             zip_file_t* entry = zip_fopen_index(archive, i, 0);
@@ -182,23 +232,37 @@ int main(int argc, char *argv[])
               printf("invalid entry");
               return 1;
             }
-            printf("Saving %s", entryinfo.name);
-            // this shit better free when it loops
             char path2[1000];
             fetch_path(path2);
             strcat(path2, entryinfo.name);
+            if (entryinfo.size == 0) {
+              // directory
+              if (access(path2, F_OK) != 0) {
+                printf("Creating directory %s\n", entryinfo.name);
+                ensure_has_folder(path2);
+                
+              }
+              continue;
+            }
+            char* directory = (char*) path_directory(path2);
+            ensure_has_folder(directory);
+            free(directory);
+            printf("Saving %s\n", entryinfo.name);
+            // this shit better free when it loops
+            
             // this will crash somewhere. too bad!
             void* buffer = malloc(entryinfo.size);
             // pray
             zip_fread(entry, buffer, entryinfo.size);
             FILE* savedfile = fopen(path2, "wb");
-            // this conversion is really fucking busted!!!
-            // fix me
-            fputs((const char*) buffer, savedfile);
+            fwrite((const void*) buffer, 1, entryinfo.size, savedfile);
             fclose(savedfile);
             free(buffer);
           }
-          printf("Done!");
+          // cleanup
+          unlink(path);
+          printf("Done!\n");
+          return 0;
         }
         
     } else {
